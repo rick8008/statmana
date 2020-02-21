689cd56b154aa349ca64b9c2d2ea747614a2cdf8
Ricardo <ricardo@novacana.com>
2020-02-21 09:57:47 -0300

    capturando dados cartas

---
+++ capturaInfo.php
@@ -0,0 +1,111 @@
+<?php
+
+function captura_deck($url){
+
+    // create curl resource
+    $ch = curl_init();
+
+    // set url
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    //return the transfer as a string
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // $output contains the output string
+    $output = curl_exec($ch);
+
+    // close curl resource to free up system resources
+    curl_close($ch);  
+    //echo $output;  
+    $deck = separa_cartas($output);
+    file_put_contents('deck.json',json_encode($deck));
+    foreach ($deck as $key => $value) {
+        captura_dados_carta($value['name']);
+    }
+
+}
+
+
+
+function captura_dados_carta($name){
+    $card_name_cache = str_replace(' ','_',$name);
+    $card_name_cache = str_replace('/','|',$card_name_cache);
+    $cache = file_exists('cache/'.$card_name_cache.'.json');
+    if(!$cache){
+        $url_card = 'https://api.scryfall.com/cards/search?q='.urlencode($name);
+
+        $ch = curl_init();
+    
+        // set url
+        curl_setopt($ch, CURLOPT_URL, $url_card);
+    
+        //return the transfer as a string
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    
+        // $output contains the output string
+        $json = curl_exec($ch);
+        $array = json_decode($json,true);
+        if($array['object'] == 'error'){
+            file_put_contents('error/'.$card_name_cache.'.json',$json);
+        }else{
+            file_put_contents('cache/'.$card_name_cache.'.json',$json);
+        }
+        echo "Getting : ".$name."\n";
+    }
+    
+}
+
+
+
+
+// seleciona parte importante do conteudo html 
+function separa_cartas($output){
+
+    $re = '/\<tbody\>(.*?)\<\/tbody\>/m';
+    preg_match_all($re, $output, $matches, PREG_SET_ORDER, 0);
+    $deck = $matches[0][0];
+    $deckstats = separa_linha($deck);
+    return $deckstats;
+
+}
+
+
+
+function separa_linha($deck){
+    $deckstats = [];
+    $re = '/\<tr\>(.*?)\<\/tr\>/m';
+    preg_match_all($re, $deck, $matches, PREG_SET_ORDER, 0);
+    foreach ($matches as $key => $value) {
+        $card = separa_coluna($value[0]);
+        if( $card != false){
+            $deckstats[] = $card;
+        }
+        
+    }
+    return $deckstats;
+}
+
+function separa_coluna($deck){
+    $re = '/\<td(.*?)\<\/td\>/m';
+    preg_match_all($re, $deck, $matches, PREG_SET_ORDER, 0);
+    if(count ($matches) == 1){
+        return false;
+    }
+    else{
+        
+        $quanty = (preg_replace("/[^0-9]/", '', strip_tags($matches[0][0])));
+        $name = separa_nome_carta($matches[1][0]);
+        $return['quanty'] = $quanty;
+        $return['name'] = $name;
+        return $return;
+    }
+   
+}
+
+function separa_nome_carta($linha){
+    $re = '/href=".\/\?view=cards\/card&card=(.*?)">/m';
+    preg_match_all($re, $linha, $matches, PREG_SET_ORDER, 0);
+    return (trim($matches[0][1]));
+}
+
+?>
--- main.php
+++ main.php
@@ -1,82 +1,11 @@
 <?php
 
-
+include 'capturaInfo.php';
  
 $url = "https://www.ligamagic.com.br/?view=dks/deck&id=1552008";
-  captura_deck($url);
-
-
-function captura_deck($url){
-
-// create curl resource
-$ch = curl_init();
-
-// set url
-curl_setopt($ch, CURLOPT_URL, $url);
-
-//return the transfer as a string
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-// $output contains the output string
-$output = curl_exec($ch);
-
-// close curl resource to free up system resources
-curl_close($ch);  
-//echo $output;  
-$deck = separa_cartas($output);
-
-var_dump(json_encode($deck));
-}
-
-
-// seleciona parte importante do conteudo html 
-function separa_cartas($output){
-
-    $re = '/\<tbody\>(.*?)\<\/tbody\>/m';
-    preg_match_all($re, $output, $matches, PREG_SET_ORDER, 0);
-    $deck = $matches[0][0];
-    $deckstats = separa_linha($deck);
-    return $deckstats;
-
-}
+captura_deck($url);
 
 
 
-function separa_linha($deck){
-    $deckstats = [];
-    $re = '/\<tr\>(.*?)\<\/tr\>/m';
-    preg_match_all($re, $deck, $matches, PREG_SET_ORDER, 0);
-    foreach ($matches as $key => $value) {
-        $card = separa_coluna($value[0]);
-        if( $card != false){
-            $deckstats[] = $card;
-        }
-        
-    }
-    return $deckstats;
-}
-
-function separa_coluna($deck){
-    $re = '/\<td(.*?)\<\/td\>/m';
-    preg_match_all($re, $deck, $matches, PREG_SET_ORDER, 0);
-    if(count ($matches) == 1){
-        return false;
-    }
-    else{
-        
-        $quanty = (preg_replace("/[^0-9]/", '', strip_tags($matches[0][0])));
-        $name = separa_nome_carta($matches[1][0]);
-        $return['quanty'] = $quanty;
-        $return['name'] = $name;
-        return $return;
-    }
-   
-}
-
-function separa_nome_carta($linha){
-    $re = '/href=".\/\?view=cards\/card&card=(.*?)">/m';
-    preg_match_all($re, $linha, $matches, PREG_SET_ORDER, 0);
-    return (trim($matches[0][1]));
-}
 
 ?>
